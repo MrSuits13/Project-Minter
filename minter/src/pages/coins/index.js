aaa00a84f3353c656ebb0daf939ab7e38d05cd25
MrSuits13 <107210589+MrSuits13@users.noreply.github.com>
2023-04-28 09:19:01 +0200

    forth Commit

--- minter/src/pages/coins/index.js
+++ minter/src/pages/coins/index.js
@@ -9,10 +9,10 @@ import { ethers } from "ethers";
 const factoryAddress = "0xB5D37d23160D2852eB99e018E6AefEa1249Dbb29"
 const factoryAbi = require("../../../artifacts/contracts/CoinFactory/CoinFactory.json");
 const coinAbi = require("../../../artifacts/contracts/Coin/Coin.json");
-export default function CreateCoin() {
+export default function coins() {
   const [walletAddress, setwalletAddress] = useState("");
+  const [ownedTokens, setOwnedTokens] = useState([{}]);
 
-  
   async function requestAccount() {
     if (window.ethereum) {
       const accounts = await window.ethereum.request({
@@ -23,14 +23,50 @@ export default function CreateCoin() {
   }
   requestAccount();
 
+  async function getTokens()
+  {
+    if (typeof window.ethereum !== "undefined") {
+        await requestAccount();
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner();
+        const factoryContract = new ethers.Contract(
+            factoryAddress,
+            factoryAbi.abi,
+            signer
+        );
 
+       const tokenArr = factoryContract.getDeployedCoins()
+       for(var i = 0; i < tokenArr.length; i++) {
+        const coinContract = new ethers.Contract(
+            tokenArr[i],
+            coinAbi.abi,
+            signer
+        )
+        if(walletAddress == "") {
+            return;
+        }
+        const ownedAmount = (await coinContract.balanceOf(walletAddress)).Number();
+        const coinName = coinContract.name()
+        const coinSymbol = coinContract.symbol()
+        if(ownedAmount > 0){
+            const token = {
+                name: coinName,
+                symbol: coinSymbol,
+                amount: ownedAmount
+
+            }
+        }
+        setOwnedTokens((ownedTokens) => [...ownedTokwns, token])
+
+       }
+    }
+  }
 
   return (
     <> 
         <Button variant="contained" color="primary" onClick={requestAccount}>
          {walletAddress ? "Connected!" : "Connect Wallet 🦊"}
         </Button>
-
     </>
   );
 }
